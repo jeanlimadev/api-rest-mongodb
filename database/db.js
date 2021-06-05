ea4b20b4a5a65f8829c0ab9929fc00178c1eab8b
Jean Lima <jeanlima3915@gmail.com>
2021-06-05 10:42:21 -0300

    alterando conexão com o banco de dados

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ function connect() {
   mongoose.set('useNewUrlParser', true)
   mongoose.set('useUnifiedTopology', true)
 
-  const MONGODB_URI = 'mongodb+srv://jeanlima3915:39151806@cluster0.vwhpu.mongodb.net/crud-products-react?retryWrites=true&w=majority'
+  const MONGODB_URI = 'mongodb+srv://jeanlimadev:MH3pcwn8prMlUOOe@crud-products-react.znobd.mongodb.net/crud-products-react?retryWrites=true&w=majority'
   
   mongoose.connect(MONGODB_URI)
   
